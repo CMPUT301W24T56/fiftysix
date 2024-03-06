52ce4b7295a6977820096ffdbbb6c77b48d31aa5
ArshArora <arsharora0388@gmail.com>
2024-03-06 14:56:08 -0700

    donw with the camera opening in profile picture just have to add the fragments and other options for user like if they want to upload picture from gallery or they want to take a new picture

--- fiftySix/app/src/main/java/com/example/fiftysix/profile_attendee_edit.java
+++ fiftySix/app/src/main/java/com/example/fiftysix/profile_attendee_edit.java
@@ -1,15 +1,16 @@
 package com.example.fiftysix;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.zxing.integration.android.IntentIntegrator;
 
 public class profile_attendee_edit extends AppCompatActivity {
     private ImageButton profile_button_change;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +21,23 @@ public class profile_attendee_edit extends AppCompatActivity {
         profile_button_change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(open_camera, 100);
             }
         });
+    }
 
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap photo = (Bitmap) extras.get("data");
+                if (photo != null) {
+                    profile_button_change.setImageBitmap(photo);
+                }
+            }
+        }
     }
 }
