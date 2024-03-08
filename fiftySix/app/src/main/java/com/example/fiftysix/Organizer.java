5df64d6feb4ab17ba7da7b0c4d2cacafdab920f8
Brady Power <156154428+bnpower@users.noreply.github.com>
2024-03-08 12:32:30 -0700

    Add files via upload

--- fiftySix/app/src/main/java/com/example/fiftysix/Organizer.java
+++ fiftySix/app/src/main/java/com/example/fiftysix/Organizer.java
@@ -29,12 +29,6 @@ import java.util.Map;
 
 import javax.security.auth.callback.Callback;
 
-/**
- * An event organizer.
- * @author Bnpower, ShikariRB
- * @see Event
- * @version 1.0
- */
 public class Organizer {
     private String organizerID;
     private ArrayList<String> eventIDs;
@@ -48,10 +42,6 @@ public class Organizer {
     private CollectionReference ref;
 
 
-    /**
-     * Constructs an organizer.
-     * @param mContext references global information about the application environment
-     */
     public Organizer(Context mContext) {
         this.mContext = mContext;
         this.organizerID = getDeviceId();
@@ -67,58 +57,34 @@ public class Organizer {
     // ________________________________METHODS_____________________________________
 
 
-    /**
-     * Gets android ID to be used as organizer ID
-     * Got from https://stackoverflow.com/questions/60503568/best-possible-way-to-get-device-id-in-android
-     * @return id the Android id of the users device
-     */
+    // Gets android ID to be used as organizer ID
+    // Got from https://stackoverflow.com/questions/60503568/best-possible-way-to-get-device-id-in-android
     public String getDeviceId() {
         String id = Settings.Secure.getString(this.mContext.getContentResolver(),
                 Settings.Secure.ANDROID_ID);
         return id;
     }
 
-    /**
-     * Gets the id of the organizer
-     * @return organizerID the Id of the organizer
-     */
     public String getOrganizerID(){
         return organizerID;
     }
 
-    /**
-     * Creates a new event in the database and generates a new check-in QR code
-     * @param details information about the event
-     * @param location the location where the event takes place
-     * @param attendeeLimit the maximum number of people that can attend the event
-     * @param eventName the name of the event
-     * @return the id of the newly created event
-     */
-    public String createEventNewQRCode( String details, String location, Integer attendeeLimit, String eventName){
-        Event event = new Event(this.organizerID, details, location, attendeeLimit, eventName, mContext);
+
+    // Method creates new event in database and generates a new check-in QR code
+    public String createEventNewQRCode( String details, String location, Integer attendeeLimit, String eventName, String date){
+        Event event = new Event(this.organizerID, details, location, attendeeLimit, eventName, date, mContext);
         addEventToOrganizerDataBase(event.getEventID());
         return event.getEventID();
     }
 
-    /**
-     * Creates a new event in the database and reuses a check-in QR code
-     * @param details information about the event
-     * @param location the location where the event takes place
-     * @param attendeeLimit the maximum number of people that can attend the event
-     * @param eventName the name of the event
-     * @param context references global information about the application environment
-     * @param oldQRID the check-in QR code to use
-     */
-    public void createEventReuseQRCode(String details, String location, Integer attendeeLimit, String eventName,Context context, String oldQRID){
-        Event event = new Event(this.organizerID, details, location, attendeeLimit, eventName, context, oldQRID);
+    // Method creates new event in database and reuses check-in QR code
+    public void createEventReuseQRCode(String details, String location, Integer attendeeLimit, String eventName, String oldQRID){
+        Event event = new Event(this.organizerID, details, location, attendeeLimit, eventName, mContext, oldQRID);
         addEventToOrganizerDataBase(event.getEventID());
     }
 
-    /**
-     * Gets a list all event data to display on the organizers home page
-     * https://stackoverflow.com/questions/50035752/how-to-get-list-of-documents-from-a-collection-in-firestore-android
-     * @exception Exception raisednwhen there is an error getting the requested documents
-     */
+    // https://stackoverflow.com/questions/50035752/how-to-get-list-of-documents-from-a-collection-in-firestore-android
+    // Returns a list of all event data to display on the organizers home page, (event name, date, location)
     public void getEventIDList() {
 
         this.myEvents = new ArrayList<>();
@@ -150,9 +116,19 @@ public class Organizer {
     // kind of like a call back used for get event IDs
     private void addEventID(String eventID){
         this.myEvents.add(eventID);
-        Log.d("Cunt", "NNNNNNNN => " + this.myEvents);
     }
 
+
+
+
+
+
+
+
+
+
+
+    // Adds organizer to database.
     private void addOrganizerToDatabase(){
         Map<String,Object> orgData = new HashMap<>();
         orgData.put("type",this.userType);
@@ -173,6 +149,8 @@ public class Organizer {
                 });
     }
 
+
+    // Adds event data to database in firestore, this is nested inside the organizer.
     private void addEventToOrganizerDataBase(String eventIDKey){
         Map<String,Object> orgEventsData = new HashMap<>();
         orgEventsData.put("testing","temp");
@@ -207,10 +185,6 @@ public class Organizer {
         });
     }
 
-    /**
-     * Gets the first event in the organizers list of events.
-     * @return the first event in the list of the organizers events.
-     */
     public String getMyEvents() {
         return myEvents.get(1);
     }
