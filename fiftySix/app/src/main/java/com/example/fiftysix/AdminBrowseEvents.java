1075a0eb1215ffef2d5a67354d53dd752d6fc165
Brady Power <156154428+bnpower@users.noreply.github.com>
2024-03-25 17:14:00 -0600

    Moving code

---
+++ fiftySix/app/src/main/java/com/example/fiftysix/AdminBrowseEvents.java
@@ -0,0 +1,72 @@
+package com.example.fiftysix;
+
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+
+public class AdminBrowseEvents extends AppCompatActivity {
+
+    private EventAdapter eventAdapter;
+    private ArrayList<Event> eventList = new ArrayList<>();
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_events);
+
+        RecyclerView recyclerView = findViewById(R.id.eventsRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        eventList = new ArrayList<>();
+        eventAdapter = new EventAdapter(eventList, false);
+        recyclerView.setAdapter(eventAdapter);
+
+        fetchEvents();
+
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void fetchEvents() {
+        db.collection("Events")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Number of events fetched: " + task.getResult().size());
+                        eventList.clear();
+
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String eventID = document.getId();
+                            String eventName = document.getString("eventName");
+                            String eventLocation = document.getString("location");
+                            String eventDate = document.getString("date");
+                            String posterURL = document.getString("posterURL");
+                            String details = document.getString("details");
+
+                            eventList.add(new Event(eventID, eventName, eventLocation, eventDate, details, 100, 1000, posterURL));
+                            eventAdapter.notifyDataSetChanged();
+                        }
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                    }
+                });
+    }
+}
